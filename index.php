b7b8ea7144386f348f92d76163fff1b50d677f74
EC2 Default User <ec2-user@ip-172-31-28-60.eu-west-1.compute.internal>
2018-10-21 01:34:29 +0000

    t

--- index.php
+++ index.php
@@ -2,23 +2,11 @@
 
 require 'vendor/autoload.php';
 
-function ServiceHandler() {
+$app->get('/tickets', function (Request $request, Response $response) {
     $data = new stdClass();
     $data->Output = "Hello World!";
 
     return json_encode($data);
-};
-
-function NewHandler() {
-    $data = new stdClass();
-    $data->Output = "Hello World!";
-
-    return json_encode($data);
-};
-
-$app = new \Slim\App;
-
-$app->get('/servicehandler', 'ServiceHandler');
-$app->get('/newhandler', 'NewHandler');
+});
 
 $app->run();
