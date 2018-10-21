a5508da24e14651931b44e55a8a73c792e66143d
EC2 Default User <ec2-user@ip-172-31-28-60.eu-west-1.compute.internal>
2018-10-21 01:25:01 +0000

    t

--- index.php
+++ index.php
@@ -9,9 +9,16 @@ function ServiceHandler() {
     return json_encode($data);
 };
 
+function NewHandler() {
+    $data = new stdClass();
+    $data->Output = "Hello World!";
+
+    return json_encode($data);
+};
+
 $app = new \Slim\App;
 
-$app->get('/', 'ServiceHandler');
-$app->post('/', 'ServiceHandler');
+$app->get('/servicehandler', 'ServiceHandler');
+$app->get('/newhandler', 'NewHandler');
 
 $app->run();
